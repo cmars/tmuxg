ce8738d183cd3d47a69366516f780952e0e61128
Casey Marshall <me@cmars.tech>
2016-02-21 22:59:57 -0600

    Add setup-script config option, improve tmux setup.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"gopkg.in/errgo.v1"
 	"gopkg.in/yaml.v2"
@@ -13,6 +15,7 @@ import (
 
 type session struct {
 	Name        string            `yaml:"name"`
+	SetupScript string            `yaml:"setup-script"`
 	Environment map[string]string `yaml:"environment"`
 	Cwd         string            `yaml:"cwd"`
 	Windows     []window          `yaml:"windows"`
@@ -43,7 +46,12 @@ func run() error {
 		return errgo.New("missing session file argument")
 	}
 
-	session, err := newSession(os.Args[1])
+	name, err := locateSession(os.Args[1])
+	if err != nil {
+		return errgo.Mask(err)
+	}
+
+	session, err := newSession(name)
 	if err != nil {
 		return errgo.Mask(err)
 	}
@@ -52,12 +60,14 @@ func run() error {
 		os.Setenv(k, os.ExpandEnv(v))
 	}
 
-	err = session.create()
+	log.Println(os.Environ())
+
+	err = session.setupScript()
 	if err != nil {
-		return errgo.Mask(err)
+		return errgo.Notef(err, "failed to execute setup script")
 	}
 
-	err = session.updateEnvironment()
+	err = session.create()
 	if err != nil {
 		return errgo.Mask(err)
 	}
@@ -89,6 +99,31 @@ func run() error {
 	return errgo.Mask(err)
 }
 
+func locateSession(name string) (string, error) {
+	if _, err := os.Stat(name); err == nil {
+		return name, nil
+	} else if !os.IsNotExist(err) {
+		return "", errgo.Notef(err, "failed to open session file %q", name)
+	}
+
+	configDir := os.Getenv("XDG_CONFIG_HOME")
+	if configDir == "" {
+		configDir = filepath.Join(os.Getenv("HOME"), ".config")
+	}
+
+	tmuxgConfigDir := filepath.Join(configDir, "tmuxg")
+	err := os.MkdirAll(tmuxgConfigDir, 0644)
+	if err != nil {
+		return "", errgo.Notef(err, "failed to create config directory %q", tmuxgConfigDir)
+	}
+
+	path := filepath.Join(tmuxgConfigDir, name+".yaml")
+	if _, err := os.Stat(path); err != nil {
+		return "", errgo.Notef(err, "failed to resolve session %q file %q", name, path)
+	}
+	return path, nil
+}
+
 func newSession(path string) (*session, error) {
 	var s session
 
@@ -104,12 +139,44 @@ func newSession(path string) (*session, error) {
 }
 
 func (s *session) tmux(args ...string) error {
-	c := exec.Command("tmux", args...)
+	c := exec.Command("tmux", append([]string{"-L", s.Name}, args...)...)
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	c.Dir = os.ExpandEnv(s.Cwd)
 	log.Printf("%v", c)
+	return errgo.Mask(c.Run())
+}
+
+func (s *session) setupScript() error {
+	if s.SetupScript == "" {
+		return nil
+	}
+
+	f, err := ioutil.TempFile("", "tmuxg-setup")
+	if err != nil {
+		return errgo.Notef(err, "failed to create temporary file for script")
+	}
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	_, err = fmt.Fprintf(f, "%s", strings.TrimSpace(s.SetupScript))
+	if err != nil {
+		return errgo.Notef(err, "failed to write temporary script file")
+	}
+	err = f.Close()
+	if err != nil {
+		return errgo.Notef(err, "error closing temporary script file")
+	}
+	err = os.Chmod(f.Name(), 0700)
+	if err != nil {
+		return errgo.Notef(err, "error setting temporary script executable")
+	}
+
+	c := exec.Command(f.Name())
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
 	return errgo.Mask(c.Run())
 }
 
@@ -122,16 +189,16 @@ func (s *session) create() error {
 	if err != nil {
 		return errgo.Notef(err, "failed to start tmux session")
 	}
-	return nil
-}
 
-func (s *session) updateEnvironment() error {
+	var keys []string
 	for k, v := range s.Environment {
-		err := s.tmux("set-environment", "-t", s.Name, k, os.ExpandEnv(v))
+		err = s.tmux("set-environment", k, os.ExpandEnv(v))
 		if err != nil {
-			return errgo.Notef(err, "failed to set session environment variables")
+			return errgo.Notef(err, "failed to set environment variable %q", k)
 		}
+		keys = append(keys, k)
 	}
+
 	return nil
 }
 
@@ -141,7 +208,7 @@ func (s *session) createWindow(i int, w *window) error {
 		cwd = s.Cwd
 	}
 	cwd = os.ExpandEnv(cwd)
-	err := s.tmux("new-window", "-t", fmt.Sprintf("%s:%d", s.Name, i),
+	err := s.tmux("new-window", "-d", "-t", fmt.Sprintf("%s:%d", s.Name, i),
 		"-c", cwd, os.ExpandEnv(w.Command))
 	if err != nil {
 		return errgo.Notef(err, "failed to create window %q", w.Name)
